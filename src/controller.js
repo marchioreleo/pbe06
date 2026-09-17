33ee2837b039aea8ed6bc9fa5baf1aaf4c4d98a6
marchioreleo <leocaninamarchiori@gmail.com>
2026-09-17 17:13:01 -0300

    update 83

--- src/controller.js
+++ src/controller.js
@@ -7,10 +7,12 @@ function autoIncrement(){
 
 const create = (req, res) => {
     const dados = req.body
-    dados.id = autoIncrement
+    dados.id = autoIncrement()
     bens.push(dados)
-    res.status(201).json(dados)
+    // res.status(201).json(dados)
+    res.redirect("http://127.0.0.1:5500/client/index.html")
 }
+
 const list = (req, res) => {
     res.json(bens)
 }
