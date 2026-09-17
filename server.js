33c47e08d5e157c558a8cfc1f7f08836ecca5580
marchioreleo <leocaninamarchiori@gmail.com>
2026-09-17 15:47:25 -0300

    mvc

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const rotaInicial = (req, res) => {
     res.json("Back-end respondendo")
 }
 
+const routes = require("./src/routes")
+
 const app = express()
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
@@ -12,6 +14,7 @@ app.use(express.json())
 const porta = 3000
 
 app.get('/', rotaInicial)
+app.use(routes)
 
 app.listen(porta, () => {
     console.log(`Servidor respondendo em: http://localhost:${porta}`)
